1503ccb5fcad72c62139298f08d62c155bd69796
venhaserjava <venhaserjava@gmail.com>
2025-01-05 20:38:50 -0400

    // Report:: 28.Desenvolvimento Microsserviço de Relatório parte 4

--- report/src/main/java/com/tecnopar/controller/OpportunityController.java
+++ report/src/main/java/com/tecnopar/controller/OpportunityController.java
@@ -1,7 +1,35 @@
 package com.tecnopar.controller;
 
+import com.tecnopar.service.OpportunityService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.ServerErrorException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.Date;
 
 @Path("/api/opportunity")
 public class OpportunityController {
+
+    @Inject
+    OpportunityService opportunityService;
+
+    @GET
+    @Path("/report")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response generateReport(){
+        try{
+            return Response.ok(opportunityService.generateCSVOpportunityReport(),
+                        MediaType.APPLICATION_OCTET_STREAM)
+                    .header("content-disposition",
+                            "attachment; filename = "+new Date()+"-- oportunidades-venda.csv").build();
+        } catch (ServerErrorException errorException) {
+            return Response.serverError().build();
+        }
+    }
+
+
 }
